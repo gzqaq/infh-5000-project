9b8918dde6a1baea995beea937af4facbdc1919f
Ziqin Gong <gongzq0301@gmail.com>
2024-11-24 15:18:34 +0800

    resize image when window is resized

--- src/gui/app.py
+++ src/gui/app.py
@@ -28,7 +28,7 @@ class App(QWidget):
 
     def _init_ui(self):
         self.setWindowTitle("A Fun App")
-        self.resize(800, 600)
+        self.resize(1080, 1440)
 
         # Widgets
         self.upload_button = QPushButton("Upload Image")
@@ -78,21 +78,43 @@ class App(QWidget):
         self.setLayout(main_layout)
 
         # Variables
-        self.image_path = None
+        self.orig_img_path = None
+        self.res_path = None
 
     def upload_image(self):
         file_path, _ = QFileDialog.getOpenFileName(
             self, "Open Image File", "", "Images (*.png *.jpg *.jpeg *.bmp)"
         )
         if file_path:
-            self.image_path = file_path
-            pixmap = QPixmap(file_path)
-            self.original_image_label.setPixmap(
-                pixmap.scaled(300, 300, Qt.KeepAspectRatio, Qt.SmoothTransformation)
+            self.orig_img_path = file_path
+            self.orig_img_pixmap = QPixmap(file_path).scaled(
+                self.original_image_label.size(),
+                Qt.KeepAspectRatio,
+                Qt.SmoothTransformation,
             )
+            self.original_image_label.setPixmap(self.orig_img_pixmap)
+
+    def resizeEvent(self, event):
+        if self.orig_img_path:
+            self.orig_img_pixmap = QPixmap(self.orig_img_path).scaled(
+                self.original_image_label.size(),
+                Qt.KeepAspectRatio,
+                Qt.SmoothTransformation,
+            )
+            self.original_image_label.setPixmap(self.orig_img_pixmap)
+
+        if self.res_path:
+            self.res_img_pixmap = QPixmap(self.res_path).scaled(
+                self.processed_image_label.size(),
+                Qt.KeepAspectRatio,
+                Qt.SmoothTransformation,
+            )
+            self.processed_image_label.setPixmap(self.res_img_pixmap)
+
+        super().resizeEvent(event)
 
     def process_image(self):
-        if not self.image_path:
+        if not self.orig_img_path:
             self.original_image_label.setText("Please upload an image first!")
             return
 
@@ -100,25 +122,17 @@ class App(QWidget):
         user_text = self.text_input.text()
 
         ############ Call the function to process the image and pass user text  ################
-        # processed_image = self.convert_to_black_and_white(self.image_path, user_text)
-        processed_image = self.communicate_with_yolo_world(self.image_path, user_text)
+        self.communicate_with_yolo_world(self.orig_img_path, user_text)
 
-        # Convert the processed image to QPixmap and display it
-        height, width, channels = processed_image.shape
-        bytes_per_line = width * channels
-        q_image = QImage(
-            processed_image.data,
-            width,
-            height,
-            bytes_per_line,
-            QImage.Format_RGB888,
-        )
-        pixmap = QPixmap.fromImage(q_image)
-        self.processed_image_label.setPixmap(
-            pixmap.scaled(300, 300, Qt.KeepAspectRatio, Qt.SmoothTransformation)
-        )
+        if self.res_path is not None:
+            self.res_img_pixmap = QPixmap(self.res_path).scaled(
+                self.processed_image_label.size(),
+                Qt.KeepAspectRatio,
+                Qt.SmoothTransformation,
+            )
+            self.processed_image_label.setPixmap(self.res_img_pixmap)
 
-    def communicate_with_yolo_world(self, img_path: str, labels: str) -> np.ndarray:
+    def communicate_with_yolo_world(self, img_path: str, labels: str) -> None:
         path_to_orig = Path(img_path).resolve()
         path_to_res = path_to_orig.with_suffix(f".res{path_to_orig.suffix}")
 
@@ -137,8 +151,7 @@ class App(QWidget):
         while path_to_res.stat().st_mtime_ns == timestamp:
             time.sleep(0.1)
 
-        img_bgr = cv2.imread(path_to_res)
-        return cv2.cvtColor(img_bgr, cv2.COLOR_BGR2RGB)
+        self.res_path = f"{path_to_res}"
 
     def convert_to_black_and_white(self, image_path, user_text):
         # Process the image using OpenCV
