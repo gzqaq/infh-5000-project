66a8ef35184f38baea9727900b48050f225608a8
Ziqin Gong <gongzq0301@gmail.com>
2024-11-24 15:02:16 +0800

    more flexible layout

--- src/gui/app.py
+++ src/gui/app.py
@@ -13,9 +13,9 @@ os.environ["QT_QPA_PLATFORM_PLUGIN_PATH"] = QLibraryInfo.location(
 
 import numpy as np
 from PyQt5.QtCore import Qt
-from PyQt5.QtGui import QFont, QImage, QPixmap
-from PyQt5.QtWidgets import (QApplication, QFileDialog, QFrame, QGridLayout,
-                             QLabel, QPushButton, QTextEdit, QVBoxLayout,
+from PyQt5.QtGui import QImage, QPixmap
+from PyQt5.QtWidgets import (QApplication, QFileDialog, QFrame, QHBoxLayout,
+                             QLabel, QLineEdit, QPushButton, QVBoxLayout,
                              QWidget)
 
 
@@ -28,64 +28,51 @@ class App(QWidget):
 
     def _init_ui(self):
         self.setWindowTitle("A Fun App")
-        self.setGeometry(100, 100, 800, 600)
-
-        # Font settings
-        font = QFont("Microsoft YaHei", 12)  # Font: Microsoft YaHei, Size: 12
-        self.setFont(font)
-
-        # Main Layout
-        main_layout = QGridLayout()
+        self.resize(800, 600)
 
         # Widgets
         self.upload_button = QPushButton("Upload Image")
-        self.upload_button.setFont(font)
         self.upload_button.clicked.connect(self.upload_image)
 
         self.submit_button = QPushButton("Submit")
-        self.submit_button.setFont(font)
         self.submit_button.setFixedWidth(
             self.upload_button.sizeHint().width()
         )  # Ensure width is consistent
         self.submit_button.clicked.connect(self.process_image)
 
-        self.text_input = QTextEdit()
-        self.text_input.setPlaceholderText("Enter some text here...")
-        self.text_input.setFixedWidth(int(self.upload_button.sizeHint().width() * 1.2))
-        self.text_input.setFont(font)
+        self.text_input = QLineEdit()
+        self.text_input.setPlaceholderText("Enter labels to detect...")
 
         # Frames for displaying images with square size
         self.original_frame = QFrame()
         self.original_frame.setFrameShape(QFrame.Box)
-        self.original_frame.setFrameShadow(QFrame.Sunken)
-        self.original_frame.setStyleSheet("background-color: lightgray;")
-        self.original_frame.setFixedSize(300, 300)  # Predefined square size
         self.original_image_label = QLabel("Original Image")
         self.original_image_label.setAlignment(Qt.AlignCenter)
-        self.original_image_label.setFont(font)
-        self.original_frame.setLayout(QVBoxLayout())
-        self.original_frame.layout().addWidget(self.original_image_label)
 
         self.processed_frame = QFrame()
         self.processed_frame.setFrameShape(QFrame.Box)
-        self.processed_frame.setFrameShadow(QFrame.Sunken)
-        self.processed_frame.setStyleSheet("background-color: lightgray;")
-        self.processed_frame.setFixedSize(300, 300)  # Predefined square size
         self.processed_image_label = QLabel("Processed Image")
         self.processed_image_label.setAlignment(Qt.AlignCenter)
-        self.processed_image_label.setFont(font)
+
+        # top layout
+        top_layout = QHBoxLayout()
+        top_layout.addWidget(self.upload_button)
+        top_layout.addWidget(self.submit_button)
+        top_layout.addWidget(self.text_input)
+
+        # image frames
+        image_layout = QHBoxLayout()
+        self.original_frame.setLayout(QVBoxLayout())
+        self.original_frame.layout().addWidget(self.original_image_label)
         self.processed_frame.setLayout(QVBoxLayout())
         self.processed_frame.layout().addWidget(self.processed_image_label)
 
-        # Add widgets to layout
-        main_layout.addWidget(
-            self.upload_button, 0, 0, 1, 1, Qt.AlignLeft | Qt.AlignTop
-        )
-        main_layout.addWidget(self.submit_button, 1, 0, 1, 1, Qt.AlignLeft)
-        main_layout.addWidget(self.text_input, 2, 0, 1, 1, Qt.AlignLeft)
-
-        main_layout.addWidget(self.original_frame, 0, 1, 3, 1, Qt.AlignCenter)
-        main_layout.addWidget(self.processed_frame, 0, 2, 3, 1, Qt.AlignCenter)
+        # main layout
+        main_layout = QVBoxLayout()
+        main_layout.addLayout(top_layout)
+        main_layout.addLayout(image_layout)
+        main_layout.addWidget(self.original_frame)
+        main_layout.addWidget(self.processed_frame)
 
         # Set layout
         self.setLayout(main_layout)
@@ -110,7 +97,7 @@ class App(QWidget):
             return
 
         # Get text input
-        user_text = self.text_input.toPlainText()
+        user_text = self.text_input.text()
 
         ############ Call the function to process the image and pass user text  ################
         # processed_image = self.convert_to_black_and_white(self.image_path, user_text)
